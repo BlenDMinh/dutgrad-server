35c9b0e8a61be16de456e6ca091048a8367a9371
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2025-04-05 20:39:17 +0700

    api create space

--- databases/entities/space_role.go
+++ databases/entities/space_role.go
@@ -9,3 +9,9 @@ type SpaceRole struct {
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 }
+
+const (
+	Owner  = 1
+	Editor = 2
+	Viewer = 3
+)
--- server/router.go
+++ server/router.go
@@ -64,7 +64,13 @@ func GetRouter() *gin.Engine {
 		spaceController := controllers.NewSpaceController()
 		spaceGroup := v1.Group("/spaces")
 		{
-			spaceController.RegisterCRUD(spaceGroup)
+			spaceGroup.GET("", spaceController.Retrieve)
+			spaceGroup.POST("", middlewares.AuthMiddleware(), spaceController.CreateSpace)
+			spaceGroup.GET("/:id", spaceController.RetrieveOne)
+			spaceGroup.PUT("/:id", spaceController.Update)
+			spaceGroup.PATCH("/:id", spaceController.Patch)
+			spaceGroup.DELETE("/:id", spaceController.Delete)
+
 			spaceGroup.GET("/public", spaceController.GetPublicSpaces)
 			spaceGroup.GET("/me", middlewares.AuthMiddleware(), userController.GetMySpaces)
 			spaceGroup.GET("/user/:user_id", userController.GetUserSpaces)
