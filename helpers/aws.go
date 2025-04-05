931422f32ecc7240006873c9253c91b35d07714f
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2025-04-05 21:37:42 +0700

    Fix AWS Session creation

--- helpers/aws.go
+++ helpers/aws.go
@@ -2,15 +2,13 @@ package helpers
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func ConnectAWS() *session.Session {
 	sess, err := session.NewSession(
 		&aws.Config{
-			Region:      aws.String("ap-southeast-1"),
-			Credentials: credentials.NewEnvCredentials(),
+			Region: aws.String("ap-southeast-1"),
 		},
 	)
 	if err != nil {
