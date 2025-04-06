e80132a900e9b218e0d85ebf9a2fe7a59c7c821b
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2025-04-06 09:08:32 +0700

    Move AWS variable to config

--- helpers/aws.go
+++ helpers/aws.go
@@ -1,14 +1,16 @@
 package helpers
 
 import (
+	"github.com/BlenDMinh/dutgrad-server/configs"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 func ConnectAWS() *session.Session {
+	config := configs.GetEnv()
 	sess, err := session.NewSession(
 		&aws.Config{
-			Region: aws.String("ap-southeast-1"),
+			Region: aws.String(config.AWS.Region),
 		},
 	)
 	if err != nil {
