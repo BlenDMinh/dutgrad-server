84fd49031513404bc6244d3605a0a9f89c127d23
Hà Nguyễn Thị Thu <102210032@sv1.dut.udn.vn>
2025-05-26 15:29:20 +0700

    limit chat input 1024

--- databases/entities/user_query.go
+++ databases/entities/user_query.go
@@ -5,7 +5,7 @@ import "time"
 type UserQuery struct {
 	ID               uint             `json:"id" gorm:"primaryKey"`
 	QuerySessionID   uint             `json:"query_session_id" gorm:"not null;index"`
-	Query            string           `json:"query" gorm:"not null"`
+	Query            string           `json:"query" gorm:"not null;size:1024"`
 	CreatedAt        time.Time        `json:"created_at"`
 	UpdatedAt        time.Time        `json:"updated_at"`
 	UserQuerySession UserQuerySession `json:"user_query_session" gorm:"foreignKey:QuerySessionID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
